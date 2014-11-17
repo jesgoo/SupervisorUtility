5ab8b021c62a0ed3de48a4276aa2613d81974170
杨晨醒 <chenxing.yang@gmail.com>
2014-11-17 20:46:59 +0800

    去除无用代码

--- test/test_program.py
+++ test/test_program.py
@@ -5,8 +5,7 @@ import time
 
 import gevent
 
-with open('a.txt', 'w') as f:
-    while True:
-        sys.stdout.write('%f %s\n' % (time.time(), sys.argv))
-        sys.stdout.flush()
-        gevent.sleep(1)
+while True:
+    sys.stdout.write('%f %s\n' % (time.time(), sys.argv))
+    sys.stdout.flush()
+    gevent.sleep(1)
